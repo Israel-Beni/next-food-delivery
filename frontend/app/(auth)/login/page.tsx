e6514fa51bc4767f16472e1b5c1d7658b3342ac3
Israel-Beni <bendegbe72@gmail.com>
2023-08-20 23:53:00 +0530

    created basic UI for login and registration page for the moment

---
+++ frontend/app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function LoginRoute(): JSX.Element {
+    return <h1>Log In</h1>;
+}
+
+export default LoginRoute;
---
+++ frontend/app/(auth)/register/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function RegisterRoute(): JSX.Element {
+    return <h1>Sign Up</h1>
+}
+
+export default RegisterRoute;
