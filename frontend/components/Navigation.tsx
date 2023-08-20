2cad50463d8280a66b7f365ec1ced693adefdc99
Apple <apple@Apples-MacBook-Air.local>
2023-08-20 22:44:50 +0530

    first commit; completed Naviagtion component for the frontend

---
+++ frontend/components/Navigation.tsx
@@ -0,0 +1,42 @@
+import React, { FC } from "react";
+import Link from "next/link";
+
+function Navigation(): JSX.Element {
+    return (
+        <nav className="container mx-auto flex justify-between p-6 px-4">
+            <div className="flex justify-between items-centers w-full">
+                <div className="xl:w-1/3">
+                    <Link
+                        className="block text-lg max-w-max text-coolGray-500 hover:text-coolGray-900 font-medium"
+                        href="/"
+                    >
+                        Food Order App
+                    </Link>
+                </div>
+
+                <div className="xl:block xl:w-1/3">
+                    <div className="flex items-center justify-end">
+                        <Link
+                            className="text-coolGray-500 hover:text-coolGray-900 font-medium"
+                            href="/"
+                        >
+                            Home
+                        </Link>
+                        <Link
+                            className="inline-block py-2 px-4 mr-2 leading-5 text-coolGray-500 hover:text-coolGray-900 bg-transparent font-medium rounded-md"
+                            href="/register"
+                        >
+                            Log In
+                        </Link>
+                        <Link
+                            className="inline-block py-2 px-4 text-sm leading-5 text-green-50 bg-green-500 hover:bg-green-600 font-medium focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 rounded-md"
+                            href="/register"
+                        >
+                            Sign Up
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
