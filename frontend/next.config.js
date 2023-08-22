cc0fad1e5ba1b2d8ce3c898ab9470916d10a29be
Israel-Beni <bendegbe72@gmail.com>
2023-08-22 21:48:48 +0530

    created Restaurant component and integrated it to the app

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        remotePatterms: [
+        remotePatterns: [
             {
                 protocol: "http",
                 hostname: "localhost",
