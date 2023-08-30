5d968a65f93dc006a0ab471db887347ec07c8426
Israel-Beni <bendegbe72@gmail.com>
2023-08-31 04:31:02 +0530

    finished with the registration logic and UI

--- frontend/components/Navigation.tsx
+++ frontend/components/Navigation.tsx
@@ -1,7 +1,20 @@
+"use client";
 import React, { FC } from "react";
 import Link from "next/link";
+import { useAppContext } from "@/contexts/AppContext";
+import Cookie from "js-cookie";
+import { useRouter } from "next/navigation";
 
 function Navigation(): JSX.Element {
+    const { user, setUser } = useAppContext();
+    const router = useRouter();
+
+    const handleLogout = () => {
+        setUser(null);
+        Cookie.remove("token");
+        router.push("/");
+    };
+
     return (
         <nav className="container mx-auto flex justify-between p-6 px-4">
             <div className="flex justify-between items-centers w-full">
@@ -22,18 +35,34 @@ function Navigation(): JSX.Element {
                         >
                             Home
                         </Link>
-                        <Link
-                            className="inline-block py-2 px-4 mr-2 leading-5 text-coolGray-500 hover:text-coolGray-900 bg-transparent font-medium rounded-md"
-                            href="/login"
-                        >
-                            Log In
-                        </Link>
-                        <Link
-                            className="inline-block py-2 px-4 text-sm leading-5 text-green-50 bg-green-500 hover:bg-green-600 font-medium focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 rounded-md"
-                            href="/register"
-                        >
-                            Sign Up
-                        </Link>
+                        {user ? (
+                            <div className="flex items-center justify-end">
+                                <span className="inline-block py-2 px-4 mr-2 leading-5 text-gray-50 hover:text-gray-100 bg-transparent font-medim rounded-md">
+                                    {user.username}
+                                </span>
+                                <button
+                                    className="incline-block py-2 px-4 text-sm leading-5 text-green-50 bg-green-500 hover:bg-green-600 font-medium focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 rounded-md"
+                                    onClick={handleLogout}
+                                >
+                                    Log Out
+                                </button>
+                            </div>
+                        ) : (
+                            <div className="flex items-center justify-end">
+                                <Link
+                                    className="inline-block py-2 px-4 mr-2 leading-5 text-coolGray-500 hover:text-coolGray-900 bg-transparent font-medium rounded-md"
+                                    href="/login"
+                                >
+                                    Log In
+                                </Link>
+                                <Link
+                                    className="inline-block py-2 px-4 text-sm leading-5 text-green-50 bg-green-500 hover:bg-green-600 font-medium focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 rounded-md"
+                                    href="/register"
+                                >
+                                    Sign Up
+                                </Link>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
